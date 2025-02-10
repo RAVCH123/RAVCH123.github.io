9dbf7a7d9650f2a9bcc3e6b2628daed6a6265785
Kento24 <kentoworkspace24@gmail.com>
2025-02-10 13:30:22 -0600

    Actualizacion de diseño y efectos

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('Dios te bendiga')
+// Función para reproducir audio
+/*
+function reproducirAudio() {
+    const audio = document.getElementById('audioPlayer');
+    audio.play().catch(error => {
+        console.log("Error al reproducir el audio: ", error);
+    });
+}
+
+window.onload = reproducirAudio;
+*/
+
+ScrollReveal().reveal('.pasajes');
+ScrollReveal().reveal('.slider__title', {delay: 150});
+ScrollReveal().reveal('.slider', {delay: 200});
+ScrollReveal().reveal('.social-media');
